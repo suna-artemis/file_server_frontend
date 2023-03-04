7d59526103fb52b8602269f6eb08e9978667e22f
suna <suan.artemis@gmail.com>
2023-03-05 07:14:18 +0900

    init🎉:  init frontend framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import { Box, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material'
+
+import { UploadFile, CreateNewFolder } from '@mui/icons-material'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box sx={{ height: '96vh', transform: 'translateZ(0px)', flexGrow: 1 }}>
+        <SpeedDial
+          sx={{ position: 'absolute', bottom: 16, right: 36 }}
+          icon={<SpeedDialIcon />}
+          ariaLabel={''}
         >
-          Learn React
-        </a>
-      </header>
+          <SpeedDialAction
+            icon={<UploadFile />}
+            tooltipTitle={'Upload file here.'}
+          />
+          <SpeedDialAction
+            icon={<CreateNewFolder />}
+            tooltipTitle={'Create new folder here'}
+          />
+        </SpeedDial>
+      </Box>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
